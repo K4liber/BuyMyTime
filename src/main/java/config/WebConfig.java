b3cc5ef17d28d7a643ef8bd04b35e95d20f65840
Jan Bielecki <janbielecki94@gmail.com>
2016-08-14 16:58:42 +0200

    Maven Spring MVC project

---
+++ src/main/java/config/WebConfig.java
@@ -0,0 +1,58 @@
+package config;
+
+import java.io.IOException;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("web")
+public class WebConfig extends WebMvcConfigurerAdapter{
+	
+	@Bean
+	public MultipartResolver multipartResolver() throws IOException {
+		return new CommonsMultipartResolver();
+	}
+	
+	//THYMELEAF CONFIG
+	
+	@Bean
+    public ServletContextTemplateResolver templateResolver() {
+    	ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver();
+    	templateResolver.setPrefix("/WEB-INF/templates/");
+    	templateResolver.setSuffix(".html");
+    	templateResolver.setTemplateMode("HTML5");
+	return templateResolver;
+    }
+	
+    @Bean    
+    public SpringTemplateEngine templateEngine() {
+    	SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+    	templateEngine.setTemplateResolver(templateResolver());
+	return templateEngine;
+    }
+    
+    @Bean
+    public ViewResolver viewResolver(){
+    	ThymeleafViewResolver viewResolver = new ThymeleafViewResolver(); 
+    	viewResolver.setTemplateEngine(templateEngine());
+    	return viewResolver;
+    }
+	
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer){
+		configurer.enable();
+	}
+	
+}
